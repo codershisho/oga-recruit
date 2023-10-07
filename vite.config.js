e892a369b8c05bfe15bf1caaf52696f896d0e25b
codershisho <codershisho@gmail.com>
2023-10-07 14:31:06 +0900

    vue/tsの環境構築

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,24 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.ts"],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: true,
-        hmr: {
-            host: "localhost",
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
+      },
+    }),
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.ts"],
+      refresh: true,
+    }),
+  ],
+  server: {
+    host: true,
+    hmr: {
+      host: "localhost",
     },
+  },
 });
